047071e329a9f06979845a475bbba125931b7548
Vivekanand Swamy Koppula <AaryavartTechSolutions@gmail.com>
2022-08-28 15:59:27 +0530

    Added Resore

--- src/MsSql/Lesson6Code/Lesson 6 Lab 2 - Using Recursive Table Expressions.sql
+++ src/MsSql/Lesson6Code/Lesson 6 Lab 2 - Using Recursive Table Expressions.sql
@@ -4,6 +4,52 @@
 USE WideWorldImporters;
 GO
 
+-- First esure the table is present.
+
+--CREATE THE SCHEMA, TABLE, and DATA-----------------
+/****************************************************
+USE WideWorldImporters
+GO
+
+CREATE SCHEMA Examples;
+GO
+
+CREATE TABLE [Examples].[Employees](
+	[empid] [int] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](20) NOT NULL,
+	[title] [nvarchar](30) NOT NULL,
+	[mgrid] [int] NULL,
+ CONSTRAINT [PK_Employees] PRIMARY KEY CLUSTERED 
+(
+	[empid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [Examples].[Employees]  WITH CHECK ADD  CONSTRAINT [FK_Employees_Employees] FOREIGN KEY([mgrid])
+REFERENCES [Examples].[Employees] ([empid])
+GO
+
+ALTER TABLE [Examples].[Employees] CHECK CONSTRAINT [FK_Employees_Employees]
+GO
+
+USE WideWorldImporters
+GO
+
+INSERT INTO Examples.Employees (name,title,mgrid)
+     VALUES
+           (N'Mickey', N'CEO',NULL),
+		   (N'Minnie', N'VP Animation',1),
+		   (N'Donald', N'Story Manager',2),
+		   (N'Daisy', N'Story Writer',3),
+		   (N'Grumpy', N'Story Research',2),
+		   (N'Happy', N'Story Development',5),
+		   (N'Dopey', N'Story Development',5);
+
+GO
+**********************************************/
+
 WITH Emp_CTE
 	AS
 	(
---
+++ src/MsSql/Restore.sql
@@ -0,0 +1,17 @@
+
+RESTORE DATABASE WideWorldImporters FROM DISK = N'C:\Users\msi\Downloads\WideWorldImporters-Standard.bak'
+
+
+RESTORE DATABASE WideWorldImporters FROM DISK = N'C:\Users\msi\Downloads\WideWorldImporters-Standard.bak' WITH 
+	MOVE 'WideWorldImporters' TO 'C:\Users\msi\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\WideWorldImporters.mdf'
+	, 
+	MOVE 'WideWorldImporters_Log' TO 'C:\Users\msi\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\WideWorldImporters_Log.ldf'
+
+
+RESTORE FILELISTONLY from disk=N'C:\Users\msi\Downloads\WideWorldImporters-Standard.bak'
+
+-- The following finally works
+RESTORE DATABASE WideWorldImporters FROM DISK = N'C:\Users\msi\Downloads\WideWorldImporters-Standard.bak' WITH 
+MOVE 'WWI_Primary' TO 'C:\Users\msi\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\WideWorldImporters.mdf'
+, MOVE 'WWI_Log' TO 'C:\Users\msi\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\WideWorldImporters_Log.ldf'
+, MOVE 'WWI_UserData'  TO 'C:\Users\msi\AppData\Local\Microsoft\Microsoft SQL Server Local DB\Instances\MSSQLLocalDB\WideWorldImporters_UserData.ndf'
